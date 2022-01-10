55b45a7f4f05af6b9cea8871c12ac12220098f54
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-01-10 15:27:29 -0800

    still working on daily problem

--- debugger.py
+++ debugger.py
@@ -1,9 +1,51 @@
-def solution2(s):
-    for i in range(len(s)):
-        if s[i] == "(":
-            start = i
-        if s[i] == ")":
-            end = i
-            return solution2(s[:start] + s[start+1:end][::-1] + s[end+1:]) #uses recursion
-    return s
-print(solution2(s="foo(bar(baz))blim"))
+def check_2_arrangement(s1, s2):
+    counter = 0
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            counter += 1
+    if counter > 1 or counter == 0:
+        return False
+    return True
+
+def chained(inputArray):
+    for i in range(len(inputArray)-1):
+        if not check_2_arrangement(inputArray[i], inputArray[i+1]):
+            return False
+    return True
+
+
+def solution(inputArray):
+    # inputArray.sort()
+    # newArray = sorted(inputArray)
+    # print(inputArray)
+    start = 0
+    for i in range(len(inputArray)-1):
+        truth_values = []
+        for j in range(start, len(inputArray)):
+            consecutive = check_2_arrangement(inputArray[i], inputArray[j])
+            truth_values.append(consecutive)
+            if consecutive:
+                inputArray[i+1], inputArray[j] = inputArray[j], inputArray[i+1]
+                start = i+1
+                break
+            else:
+                inputArray[i], inputArray[j] = inputArray[j], inputArray[i]
+        if True not in truth_values:
+            return False
+    print(inputArray)
+    return chained(inputArray)
+
+print()
+# print(solution(["aa", "ab", "bb"])) #True
+# print(solution(["aba", "bbb", "bab"])) #False
+print(solution(["ab", "bb", "aa"])) #True 
+# print(solution(["q", "q"])) #False
+# print(solution(["abc", "bef", "bcc", "bec", "bbc", "bdc"])) #True -> ["abc", "bbc", "bdc", "bcc", "bec", "bef"]
+
+# print(solution(["ff", "gf", "af", "ar", "hf"])) #True
+
+# print(solution(["ab", "ad", "ef", "eg"])) #False
+# print(solution(["abc", "abx", "axx", "abc"])) #False
+
+# print(solution(["abc", "abx", "axx", "abx", "abc"])) #True
+print()
