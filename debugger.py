f5eea4e9a166db816fdc7c2966a4d5ab270124aa
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-01-10 16:03:52 -0800

    used alternate solutions to learn about depth first search recursion and permutations library

--- debugger.py
+++ debugger.py
@@ -1,51 +1,35 @@
-def check_2_arrangement(s1, s2):
-    counter = 0
+def alt_solution_help(inputArray):
+    for i in range(len(inputArray)):
+        if isChainPossible(i, inputArray):
+            return True
+    return False
+
+def isChainPossible(start, inputArray):
+    if len(inputArray) == 1:
+        return True
+    newArray = inputArray[:start] + inputArray[(1 + start):]
+    for i in range(len(newArray)):
+        if differsByOne(inputArray[start], newArray[i]) and isChainPossible(i, newArray):
+            return True
+    return False
+
+def differsByOne(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    differentChars = 0
     for i in range(len(s1)):
         if s1[i] != s2[i]:
-            counter += 1
-    if counter > 1 or counter == 0:
-        return False
-    return True
-
-def chained(inputArray):
-    for i in range(len(inputArray)-1):
-        if not check_2_arrangement(inputArray[i], inputArray[i+1]):
-            return False
-    return True
-
-
-def solution(inputArray):
-    # inputArray.sort()
-    # newArray = sorted(inputArray)
-    # print(inputArray)
-    start = 0
-    for i in range(len(inputArray)-1):
-        truth_values = []
-        for j in range(start, len(inputArray)):
-            consecutive = check_2_arrangement(inputArray[i], inputArray[j])
-            truth_values.append(consecutive)
-            if consecutive:
-                inputArray[i+1], inputArray[j] = inputArray[j], inputArray[i+1]
-                start = i+1
-                break
-            else:
-                inputArray[i], inputArray[j] = inputArray[j], inputArray[i]
-        if True not in truth_values:
-            return False
-    print(inputArray)
-    return chained(inputArray)
+            differentChars += 1
+    return differentChars == 1
 
 print()
-# print(solution(["aa", "ab", "bb"])) #True
-# print(solution(["aba", "bbb", "bab"])) #False
-print(solution(["ab", "bb", "aa"])) #True 
-# print(solution(["q", "q"])) #False
-# print(solution(["abc", "bef", "bcc", "bec", "bbc", "bdc"])) #True -> ["abc", "bbc", "bdc", "bcc", "bec", "bef"]
-
-# print(solution(["ff", "gf", "af", "ar", "hf"])) #True
-
-# print(solution(["ab", "ad", "ef", "eg"])) #False
-# print(solution(["abc", "abx", "axx", "abc"])) #False
-
-# print(solution(["abc", "abx", "axx", "abx", "abc"])) #True
+print(alt_solution_help(["aa", "ab", "bb"])) #True
+print(alt_solution_help(["aba", "bbb", "bab"])) #False
+print(alt_solution_help(["ab", "bb", "aa"])) #True 
+print(alt_solution_help(["q", "q"])) #False
+print(alt_solution_help(["abc", "bef", "bcc", "bec", "bbc", "bdc"])) #True -> ["abc", "bbc", "bdc", "bcc", "bec", "bef"]
+print(alt_solution_help(["ff", "gf", "af", "ar", "hf"])) #True
+print(alt_solution_help(["ab", "ad", "ef", "eg"])) #False
+print(alt_solution_help(["abc", "abx", "axx", "abc"])) #False
+print(alt_solution_help(["abc", "abx", "axx", "abx", "abc"])) #True
 print()
