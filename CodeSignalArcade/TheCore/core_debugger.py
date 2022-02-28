1c5d083806030bfb439460f4d986a46e869a6a13
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-02-28 12:29:49 -0800

    solved daily problem

--- CodeSignalArcade/TheCore/core_debugger.py
+++ CodeSignalArcade/TheCore/core_debugger.py
@@ -51,4 +51,21 @@ def solution(commands):
             same += 1
     return same
 
-print(solution("LLARL")) #3
+# print(solution("LLARL")) #3
+
+def solution(candlesNumber, makeNew):
+    candles = candlesNumber
+    leftovers = 0
+    while (candlesNumber+leftovers) >= makeNew:
+        temp = candlesNumber
+        candlesNumber = (candlesNumber+leftovers)//makeNew
+        leftovers = (leftovers+temp)%makeNew
+        candles += candlesNumber
+        if leftovers == 0 and (candlesNumber+leftovers) < makeNew:
+            break
+    return candles
+
+print(solution(5,2)) #9
+print(solution(15, 5)) #18
+print(solution(14, 3)) #20
+print(solution(12, 2)) #23
