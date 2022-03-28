682d809552ad9a5882cf08647f84d1d0177dff74
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-03-27 23:57:04 -0700

    update

--- LeetCode/longestPalindromeString.py
+++ LeetCode/longestPalindromeString.py
@@ -9,21 +9,23 @@ class Solution:
     #                     max_sub = substring
     #     return max_sub
     
-    def longestPalindrome(self, s: str, max_str="") -> str:
-        if len(s) == 0:
-            return max_str
-        if len(s) == 1 and len(s) > len(max_str):
-            return s
-        if s == s[::-1] and len(s) > len(max_str):
-            return s
-        end = len(s)
-        for i in range(len(s)):
-            if len(max_str) >= len(s[i:end-1]):
-                return max_str
-            else:
-                max_str = self.longestPalindrome(s[i:end-1], max_str)
-            end = len(s)+1
-        return max_str
+    # def longestPalindrome(self, s: str, max_str="") -> str:
+    #     if len(s) == 0:
+    #         return max_str
+    #     if len(s) == 1 and len(s) > len(max_str):
+    #         return s
+    #     if s == s[::-1] and len(s) > len(max_str):
+    #         return s
+    #     end = len(s)
+    #     for i in range(len(s)):
+    #         if len(max_str) >= len(s[i:end-1]):
+    #             return max_str
+    #         else:
+    #             max_str = self.longestPalindrome(s[i:end-1], max_str)
+    #         end = len(s)+1
+    #     return max_str
+    
+    #create solution that finds biggest substring among original and flipped
 
 
 temp = Solution()
