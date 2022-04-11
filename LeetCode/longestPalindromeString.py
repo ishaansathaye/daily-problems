0328442ebd5913760a68a470be6dc065fd8182f7
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-04-10 23:21:50 -0700

    still sovling longest palendrmoic string

--- LeetCode/longestPalindromeString.py
+++ LeetCode/longestPalindromeString.py
@@ -16,13 +16,29 @@ class Solution:
         return max_str
     
     #create solution that finds biggest substring among original and flipped
+    def longestPalindrome(self, s: str, max_str="") -> str:
+        flipped = s[::-1]
+        ans_long = ""
+        pal = ""
+        curr_char = {}
+
+        i = 0
+        for j in range(len(s)):
+            if s[j] in curr_char:
+                i = max(curr_char[s[j]], i)
+            if len(s[i:j+1]) > len(ans_long):
+                ans_long = s[i:j+1]
+                if s[i:j+1] == flipped[i:j+1]:
+                    pal = s[i:j+1]
+            curr_char[s[j]] = j+1
+        return pal
 
 
 temp = Solution()
-# print(temp.longestPalindrome("babad"))
-# print(temp.longestPalindrome("a"))
-# print(temp.longestPalindrome("bb"))
-# print(temp.longestPalindrome("cbbd"))
-# print(temp.longestPalindrome("abb"))
-# print(temp.longestPalindrome("xaabacxcabaaxcabaax"))
+print(temp.longestPalindrome("babad"))
+print(temp.longestPalindrome("a"))
+print(temp.longestPalindrome("bb"))
+print(temp.longestPalindrome("cbbd"))
+print(temp.longestPalindrome("abb"))
+print(temp.longestPalindrome("xaabacxcabaaxcabaax"))
 print(temp.longestPalindrome("abbcccbbbcaaccbababcbcabca"))
