9da4222608fa2f90bec2e8e069183ee834aadea3
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-04-10 22:22:25 -0700

    working on leet code problem

--- LeetCode/longestPalindromeString.py
+++ LeetCode/longestPalindromeString.py
@@ -1,14 +1,4 @@
 class Solution:
-    # def longestPalindrome(self, s: str) -> str:
-    #     max_sub = s[0]
-    #     if len(s) > 1:
-    #         for i in range(len(s)):
-    #             for j in range(i+1, len(s)):
-    #                 substring = s[i:j+1]
-    #                 if substring == substring[::-1] and len(substring) > 1 and len(substring) > len(max_sub):
-    #                     max_sub = substring
-    #     return max_sub
-    
     def longestPalindrome(self, s: str, max_str="") -> str:
         if len(s) == 0:
             return max_str
