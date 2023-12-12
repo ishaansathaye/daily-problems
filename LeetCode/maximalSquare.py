91cd7dca3d3a012f2b24e94e70752780bbd9b815
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2023-12-12 11:50:18 -0800

    added maximal square

---
+++ LeetCode/maximalSquare.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maximalSquare(self, matrix: list[list[str]]) -> int:
+        if matrix is None or len(matrix) < 1:
+            return 0
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        # dp = [[0]*(cols+1) for _ in range(rows+1)]
+        dp = []
+        for i in range(rows+1):
+            row = []
+            for j in range(cols + 1):
+                row.append(0)
+            dp.append(row)
+        max_side = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == '1':
+                    dp[r+1][c+1] = min(dp[r][c], dp[r+1][c], dp[r][c+1]) + 1
+                    # Be careful of the indexing since dp grid has additional row and column
+                    max_side = max(max_side, dp[r+1][c+1])
+
+        return max_side * max_side
+
+
+if __name__ == "__main__":
+    matrix = [["1", "0", "1", "0", "0"],
+              ["1", "0", "1", "1", "1"],
+              ["1", "1", "1", "1", "1"],
+              ["1", "0", "0", "1", "0"]]
+    s = Solution()
+    print(s.maximalSquare(matrix))
