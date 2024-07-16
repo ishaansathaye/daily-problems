d7444f23ea9a1cb426999aa7713388cdcaa06eca
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2024-07-15 20:12:44 -0700

    bfs

--- LeetCode/canFinish.py
+++ LeetCode/canFinish.py
@@ -1,3 +1,49 @@
+from typing import List
+
+
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pass
+    def canFinish(self, numCourses: int,
+                  prerequisites: List[List[int]]) -> bool:
+        '''BFS Topological Manner'''
+        hMap = {}  # integer and list of dependents on integer
+        inDegrees = [0 for i in range(numCourses)]  # length is num of courses
+
+        #
+        for pre in prerequisites:
+            inD = pre[0]  # dependent
+            out = pre[1]  # indep
+            inDegrees[inD] += 1
+            if out not in hMap:
+                hMap[out] = []
+            # add deps to the indep key
+            hMap[out].append(inD)
+
+        # iterate over inDegrees arr
+        queue = []  # of integers
+        count = 0  # how many elem in queue
+        for i in range(numCourses):
+            if inDegrees[i] == 0:
+                queue.append(i)
+                count += 1
+
+        # all the courses are done
+        if count == numCourses:
+            return True
+        # nothing has gone in the queue
+        if count == 0:
+            return False
+
+        # bfs
+        while len(queue) != 0:
+            parent = queue.pop(0)  # parent out queue
+            if parent in hMap:
+                children = hMap[parent]  # get children
+                for child in children:
+                    inDegrees[child] -= 1
+                    # once indegree is 0 then put into queue
+                    if inDegrees[child] == 0:
+                        queue.append(child)
+                        count += 1
+                        if count == numCourses:
+                            return True
+        return False
--- LeetCode/levelOrder.py
+++ LeetCode/levelOrder.py
@@ -1,9 +1,54 @@
+from typing import List, Optional
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
+    def _levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        if root is None:
+            return result
+        queue = []  # of tree nodes
+        queue.append(root)
+        while len(queue) != 0:
+            size = len(queue)
+            li = []
+            # how many elements at that level
+            for i in range(size):
+                curr = queue.pop(0)
+                li.append(curr.val)
+                if curr.left is not None:
+                    queue.append(curr.left)
+                if curr.right is not None:
+                    queue.append(curr.right)
+            result.append(li)
+        return result
+
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
+        '''DFS Approach -> TC: O(n), SC: O(1)'''
+        result = []
+        if root is None:
+            return result
+        self.dfs(root, 0, result)
+        return result
+
+    def dfs(self, root, level, result):
+        # base
+        if root is None:
+            return
+        # logic
+        if len(result) == level:
+            # make sure list is there at that level
+            result.append([])
+        result[level].append(root.val)
+        # left call
+        self.dfs(root.left, level+1, result)
+        # right call
+        self.dfs(root.right, level+1, result)
