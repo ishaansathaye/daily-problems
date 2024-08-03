49df2d169e84147673d6033285fec6cf82c0def6
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2024-08-02 17:57:28 -0700

    expression add operators

--- LeetCode/addOperators.py
+++ LeetCode/addOperators.py
@@ -5,12 +5,12 @@ class Solution:
     def __init__(self):
         self.res = []
 
-    def addOperators(self, num: str, target: int) -> List[str]:
+    def _addOperators(self, num: str, target: int) -> List[str]:
         '''Without Backtracking (Creating new Strings Everytime)'''
         self.helper(num, 0, "", 0, 0, target)
         return self.res
 
-    def helper(self, num, pivot, path, calc, tail, target):
+    def _helper(self, num, pivot, path, calc, tail, target):
         # base
         if pivot == len(num):
             if calc == target:
@@ -38,3 +38,49 @@ class Solution:
                 # *
                 self.helper(num, i+1, path+"*"+str(curr), calc -
                             tail+(tail*curr), tail*curr, target)
+
+    def addOperators(self, num: str, target: int) -> List[str]:
+        '''Backtracking'''
+        self.helper(num, 0, [], 0, 0, target)
+        return self.res
+
+    def helper(self, num, pivot, path, calc, tail, target):
+        # base
+        if pivot == len(num):
+            if calc == target:
+                self.res.append("".join(path))
+            return
+        # logic
+        # creating the level:
+        for i in range(pivot, len(num)):
+            # preceding zero case
+            # only when i is further ahead: "05"
+            if num[pivot] == "0" and i != pivot:
+                continue
+            # ex: 1, 12, 123
+            curr = int(num[pivot:i+1])
+            if pivot == 0:
+                # at the top level
+                path.append(str(curr))
+                self.helper(num, i+1, path, curr, curr, target)
+                path.pop()
+            else:
+                # +
+                path.append("+")
+                path.append(str(curr))
+                self.helper(num, i+1, path, calc+curr, curr, target)
+                path.pop()
+                path.pop()
+                # -
+                path.append("-")
+                path.append(str(curr))
+                self.helper(num, i+1, path, calc-curr, -curr, target)
+                path.pop()
+                path.pop()
+                # *
+                path.append("*")
+                path.append(str(curr))
+                self.helper(num, i+1, path, calc-tail +
+                            (tail*curr), tail*curr, target)
+                path.pop()
+                path.pop()
