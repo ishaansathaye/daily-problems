2e1946a174352625e9c374d047521d00020a1cbc
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2025-02-13 17:07:30 -0800

    init reverse linked list 2

---
+++ LeetCode/s30Mock/mock17/mock17-1.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int,
+                       right: int) -> Optional[ListNode]:
+        pass
